059010dacc33023bd06e06f6aa2caa77c3e239aa
Ramanathan <ramanathal@gmail.com>
2022-05-31 00:48:57 +0530

    For testing changesd env file for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.aryaa.one'
+  //apiUrl: 'https://api.aryaa.one'
+  apiUrl: 'https://ec2-13-126-45-140.ap-south-1.compute.amazonaws.com/api/v1'
 };
